ed371a64f2ac7856ff961a5addfc0a3c64aa7b13
Abdulrasaq Taofeeq <abdulrasaqolarewaju88@gmail.com>
2025-10-06 07:15:59 +0100

    [fix]: remove dotenv config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,8 +1,5 @@
-import { configDotenv } from "dotenv";
-const process = configDotenv()
-
 export const appConfig = {
-  STORE_ORDER_CONTACT: Number(process.env.WHATSAPP_NUMBER),
-  PAYSTACK_PUBLIC_KEY: String(process.env.PAYSTACK_PUBLIC_KEY),
-  FLUTTERWAVE_PUBLIC_KEY: String(process.env.FLUTTERWAVE_PUBLIC_KEY)
+  STORE_ORDER_CONTACT: Number(import.meta.env.VITE_WHATSAPP_NUMBER),
+  PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY || "",
+  FLUTTERWAVE_PUBLIC_KEY: import.meta.env.VITE_FLUTTERWAVE_PUBLIC_KEY || "",
 };
